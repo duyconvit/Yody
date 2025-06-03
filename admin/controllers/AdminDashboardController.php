2216c570d303c9935bf7e67453148eebce4fbc20
tqluc12 <trinhquangluc2005@gmail.com>
2025-06-03 20:58:09 +0700

    thong ke

--- admin/controllers/AdminDashboardController.php
+++ admin/controllers/AdminDashboardController.php
@@ -1,7 +1,30 @@
 <?php
 
-class AdminDashboardController {
-    public function trangChu() {
-        require_once "./views/trangChuAdmin.php";
+require_once __DIR__ . '/../models/AdminDonHang.php';
+require_once __DIR__ . '/../models/AdminTaiKhoan.php';
+
+class AdminDashboardController  
+{
+    public $modelTaiKhoan;
+    public $modelDonHang;
+
+    public function __construct()
+    {
+        $this->modelTaiKhoan = new AdminTaiKhoan();
+        $this->modelDonHang = new AdminDonHang();
     }
+
+    public function trangchu()
+    {
+        // $listDonHang = $this->modelDonHang->getAllDetailDonHang();
+        $listAllDonHang = $this->modelDonHang->getAllDonHang();
+        $listTaiKhoan = $this->modelTaiKhoan->getAllTaiKhoanThongKe();
+        $tongThuNhap = $this->modelDonHang->tongThuNhap();
+        // var_dump($tongThuNhap);die();
+        // $listDonHang = $this->modelDonHang->getAllDonHangDetail();
+
+        $listDetailDonHang = $this->modelDonHang->getAllDetailDonHangSanPhamBanChay();
+        require_once './views/trangChuAdmin.php';
+    }
+
 }
--- admin/models/AdminTaiKhoan.php
+++ admin/models/AdminTaiKhoan.php
@@ -174,4 +174,19 @@ class AdminTaiKhoan{
 
         }
     }
+    public function getAllTaiKhoanThongKe()
+    {
+        try {
+
+            $sql = "SELECT * FROM tai_khoans";
+
+            $stmt = $this->conn->prepare($sql);
+
+            $stmt->execute();
+
+            return $stmt->fetchAll();
+        } catch (Exception $e) {
+            echo "Lỗi: " . $e->getMessage();
+        }
+    }
 }
