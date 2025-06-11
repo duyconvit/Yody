024072b74fff35bb64b94dc41b47a01661e4f912
Quang Nam <quangnam8386@gmail.com>
2025-06-11 16:26:04 +0700

    fix tìm sản phẩm

--- admin/controllers/AdminDashboardController.php
+++ admin/controllers/AdminDashboardController.php
@@ -3,10 +3,12 @@
 class AdminDashboardController  
 {
     public $modelDonHang;
+    public $modelTaiKhoan;
 
     public function __construct()
     {
         $this->modelDonHang = new AdminDonHang();
+        $this->modelTaiKhoan = new AdminTaiKhoan();
     }
 
     public function trangchu()
@@ -14,6 +16,7 @@ class AdminDashboardController
         // $listDonHang = $this->modelDonHang->getAllDetailDonHang();
         $listAllDonHang = $this->modelDonHang->getAllDonHang();
         $tongThuNhap = $this->modelDonHang->tongThuNhap();
+        $listTaiKhoan = $this -> modelTaiKhoan->getAllTaiKhoan(2);
         // var_dump($tongThuNhap);die();
         // $listDonHang = $this->modelDonHang->getAllDonHangDetail();
 
